7e538ac9e49dddc771ff6bcf8d1654c599d9bcfc
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-12-09 21:57:59 +0900

    Add DefaultValueUtil class

---
+++ garum/src/main/java/com/os/operando/garum/utils/DefaultValueUtil.java
@@ -0,0 +1,82 @@
+package com.os.operando.garum.utils;
+
+import android.content.res.Resources;
+
+import com.os.operando.garum.ResId;
+import com.os.operando.garum.annotations.DefaultBoolean;
+import com.os.operando.garum.annotations.DefaultFloat;
+import com.os.operando.garum.annotations.DefaultInt;
+import com.os.operando.garum.annotations.DefaultLong;
+import com.os.operando.garum.annotations.DefaultString;
+import com.os.operando.garum.annotations.DefaultStringSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DefaultValueUtil {
+
+    public static int getDefaulIntValue(DefaultInt defaultInt, Resources resources) {
+        if (defaultInt == null) {
+            return 0;
+        }
+        int resId = defaultInt.redId();
+        if (isResIdDefault(resId)) {
+            return defaultInt.value();
+        }
+        return resources.getInteger(resId);
+    }
+
+    public static String getDefaulStringValue(DefaultString defaultString, Resources resources) {
+        if (defaultString == null) {
+            return null;
+        }
+        int resId = defaultString.redId();
+        if (isResIdDefault(resId)) {
+            return defaultString.value();
+        }
+        return resources.getString(resId);
+    }
+
+    public static boolean getDefaulBooleanValue(DefaultBoolean defaultBoolean, Resources resources) {
+        if (defaultBoolean == null) {
+            return false;
+        }
+        int resId = defaultBoolean.redId();
+        if (isResIdDefault(resId)) {
+            return defaultBoolean.value();
+        }
+        return resources.getBoolean(resId);
+    }
+
+    public static long getDefaulLongValue(DefaultLong defaultLong) {
+        if (defaultLong == null) {
+            return 0;
+        }
+        return defaultLong.value();
+    }
+
+    public static float getDefaulFloatValue(DefaultFloat defaultFloat) {
+        if (defaultFloat == null) {
+            return 0.0f;
+        }
+        return defaultFloat.value();
+    }
+
+    public static Set<String> getDefaultSetValue(DefaultStringSet defaultStringSet, Resources
+            resources) {
+        if (defaultStringSet == null) {
+            return new HashSet<String>();
+        }
+        int resId = defaultStringSet.redId();
+        if (isResIdDefault(resId)) {
+            return new HashSet<String>(Arrays.asList(defaultStringSet.value()));
+        }
+        return new HashSet<String>(
+                Arrays.asList(resources.getStringArray(resId)));
+    }
+
+    private static boolean isResIdDefault(int resId) {
+        return resId == ResId.DEFAULT_VALUE;
+    }
+}
