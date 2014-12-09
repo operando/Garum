a94d5eb54446222264955705e33398c5a0c111b0
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-12-09 22:02:35 +0900

    Modified to use the DefaultValueUtil

--- garum/src/main/java/com/os/operando/garum/models/PrefModel.java
+++ garum/src/main/java/com/os/operando/garum/models/PrefModel.java
@@ -14,12 +14,12 @@ import com.os.operando.garum.annotations.DefaultString;
 import com.os.operando.garum.annotations.DefaultStringSet;
 import com.os.operando.garum.serializers.TypeSerializer;
 import com.os.operando.garum.utils.Cache;
+import com.os.operando.garum.utils.DefaultValueUtil;
 import com.os.operando.garum.utils.GarumLog;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -80,69 +80,24 @@ public abstract class PrefModel {
                 } else {
                     if (fieldType.equals(Integer.class) || fieldType.equals(int.class)) {
                         final DefaultInt defaultInt = field.getAnnotation(DefaultInt.class);
-                        if (defaultInt == null) {
-                            field.set(this, 0);
-                        } else {
-                            int resId = defaultInt.redId();
-                            if (resId == -1) {
-                                field.set(this, defaultInt.value());
-                            } else {
-                                field.set(this, resources.getInteger(resId));
-                            }
-                        }
+                        field.set(this, DefaultValueUtil.getDefaulIntValue(defaultInt, resources));
                     } else if (fieldType.equals(String.class)) {
                         final DefaultString defaultString = field.getAnnotation(DefaultString.class);
-                        if (defaultString == null) {
-                            field.set(this, null);
-                        } else {
-                            int resId = defaultString.redId();
-                            if (resId == -1) {
-                                field.set(this, defaultString.value());
-                            } else {
-                                field.set(this, context.getString(resId));
-                            }
-                        }
+                        field.set(this, DefaultValueUtil.getDefaulStringValue(defaultString, resources));
                     } else if (fieldType.equals(Boolean.class) || fieldType.equals(boolean.class)) {
                         final DefaultBoolean defaultBoolean = field.getAnnotation(DefaultBoolean.class);
-                        if (defaultBoolean == null) {
-                            field.set(this, false);
-                        } else {
-                            int resId = defaultBoolean.redId();
-                            if (resId == -1) {
-                                field.set(this, defaultBoolean.value());
-                            } else {
-                                field.set(this, resources.getBoolean(resId));
-                            }
-                        }
+                        field.set(this, DefaultValueUtil.getDefaulBooleanValue(defaultBoolean, resources));
                     } else if (fieldType.equals(Long.class) || fieldType.equals(long.class)) {
                         final DefaultLong defaultLong = field.getAnnotation(DefaultLong.class);
-                        if (defaultLong == null) {
-                            field.set(this, 0);
-                        } else {
-                            field.set(this, defaultLong.value());
-                        }
+                        field.set(this, DefaultValueUtil.getDefaulLongValue(defaultLong));
                     } else if (fieldType.equals(Float.class) || fieldType.equals(float.class)) {
                         final DefaultFloat defaultFloat = field.getAnnotation(DefaultFloat.class);
-                        if (defaultFloat == null) {
-                            field.set(this, 0.0F);
-                        } else {
-                            field.set(this, defaultFloat.value());
-                        }
+                        field.set(this, DefaultValueUtil.getDefaulFloatValue(defaultFloat));
                     } else if (fieldType.equals(Set.class) && field.getGenericType() instanceof ParameterizedType) {
                         ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
                         if (Arrays.asList(parameterizedType.getActualTypeArguments()).contains(String.class)) {
                             final DefaultStringSet defaultStringSet = field.getAnnotation(DefaultStringSet.class);
-                            if (defaultStringSet == null) {
-                                field.set(this, new HashSet<String>());
-                            } else {
-                                int resId = defaultStringSet.redId();
-                                if (resId == -1) {
-                                    field.set(this, new HashSet<String>(Arrays.asList(defaultStringSet.value())));
-                                } else {
-                                    field.set(this, new HashSet<String>(
-                                            Arrays.asList(resources.getStringArray(resId))));
-                                }
-                            }
+                            field.set(this, DefaultValueUtil.getDefaultSetValue(defaultStringSet, resources));
                         }
                     }
                 }
