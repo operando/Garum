6d37325ee56b622c5a15b38d6280ffc555af027d
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-04-12 23:59:24 +0900

    Delete mode

--- app/src/main/java/com/os/operando/garum/sample/models/PrefTest.java
+++ app/src/main/java/com/os/operando/garum/sample/models/PrefTest.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 
 @Data
 @ToString
-@Pref(name = "pref_test", mode = Pref.MODE.MODE_PRIVATE)
+@Pref(name = "pref_test")
 public class PrefTest extends PrefModel {
 
     @PrefKey
